ff91bb44474ac16af0671f128a8752012efd519a
fuok <liuyang@Browns-Mac.local>
2016-03-26 21:10:28 +0800

    增加了敌鸟。。。

--- Assets/Scrips/Enemy.cs
+++ Assets/Scrips/Enemy.cs
@@ -1,17 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 	public float moveSpeed;
-	// Use this for initialization
-	void Start () {
-	
+	private float mFlightRange;
+	private bool flyUp;
+	//保存初始位置属性
+	private float mPosY{ set; get; }
+
+	void Start ()
+	{
+		//上下飞行距离
+		mFlightRange = 1f;
+		//保存初始高度
+		mPosY = this.transform.position.y;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+	{
 		if (GameManager.status == GameStatus.Playing) {
-			this.transform.Translate (Vector2.left * moveSpeed * Time.deltaTime);
+//			bool flyUp = true;
+			print (mPosY);
+			if (this.transform.position.y > mPosY + mFlightRange) {
+//				print ("到达上界:" + this.transform.position.y);
+				flyUp = false;
+			} else if (this.transform.position.y < mPosY - mFlightRange) {
+//				print ("到达下界:" + this.transform.position.y);
+				flyUp = true;
+			}
+
+			if (flyUp) {
+//				print ("上");
+				this.transform.Translate (new Vector3 (Vector2.left.x, Vector2.up.y, 0) * moveSpeed * Time.deltaTime);
+			} else {
+//				print ("下");
+				this.transform.Translate (new Vector3 (Vector2.left.x, Vector2.down.y, 0) * moveSpeed * Time.deltaTime);
+			}
+				
+			//销毁
 			if (this.gameObject.transform.position.x < -6.54) {
 				Destroy (this.gameObject);
 			}
--- Assets/Scrips/PropCreater.cs
+++ Assets/Scrips/PropCreater.cs
@@ -7,17 +7,15 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class PropCreater : MonoBehaviour
 {
-	public GameObject propPrefabs,enemyPrefabs;
-//	private GameObject mPrefabs;
+	public GameObject propPrefabs, enemyPrefabs;
 
 	void Start ()
 	{
-//		print(SceneManager.GetActiveScene().name);
 		//创建第一个
-		if ("GameScene1".Equals(SceneManager.GetActiveScene().name)) {
-			GameObject.Instantiate (propPrefabs, new Vector3(3.83f,0.61f,0), Quaternion.identity);
-		}else if ("GameScene2".Equals(SceneManager.GetActiveScene().name)) {
-//			GameObject.Instantiate (enemyPrefabs, new Vector3(3.83f,0.61f,0), Quaternion.identity);
+		if ("GameScene1".Equals (SceneManager.GetActiveScene ().name)) {
+			GameObject.Instantiate (propPrefabs, new Vector3 (3.83f, 0.61f, 0), Quaternion.identity);
+		} else if ("GameScene2".Equals (SceneManager.GetActiveScene ().name)) {
+			GameObject.Instantiate (enemyPrefabs, new Vector3 (5.45f, 0.55f, 0), Quaternion.identity);
 		}
 	}
 
@@ -25,6 +23,9 @@ public class PropCreater : MonoBehaviour
 	{
 	
 	}
+
+	//控制复制敌鸟的频率
+	private float mRate = 2.5f;
 
 	void OnTriggerExit2D (Collider2D collider)//触发后复制水管
 	{
@@ -34,9 +35,11 @@ public class PropCreater : MonoBehaviour
 //			GameObject.Instantiate (collider.transform.parent.gameObject, v3, collider.transform.rotation);//注意是parent，上下水管是整体
 			GameObject.Instantiate (propPrefabs, v3, collider.transform.rotation);//也可以复制预设，避免克隆的名字越来越长
 			//还可以用水管重用的方法，没必要反复创建销毁，,TODO
-		}else if (collider.tag.Equals ("Enemy")) {
-			Vector3 v3 = new Vector3 (collider.transform.position.x + 2.5f, Random.Range (-2f, 3f), collider.transform.position.z);
+		} else if (collider.tag.Equals ("Enemy")) {
+			Vector3 v3 = new Vector3 (collider.transform.position.x + Mathf.Clamp (mRate, 0.2f, mRate), Random.Range (-2f, 3.5f), collider.transform.position.z);
 			GameObject.Instantiate (enemyPrefabs, v3, collider.transform.rotation);
+			//增加点难度...
+			mRate -= 0.05f;
 		}
 
 	}
